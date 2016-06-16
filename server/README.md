46eaad29353f131fbc7d65794ef99f6e8eb5cad7
Andrew Jackura <ajackura@google.com>
2016-06-16 13:08:36 -0700

    Fix server example and update README (#14)

--- server/README.md
+++ server/README.md
@@ -3,8 +3,9 @@
 This is a simple example of what a GooGet server looks like. 
 The server looks for a folder in it's root directory called 'packages', 
 creating it if necesary. The directory contents are read on a set 
-interval and all .goo packages served in the repo (localhost:8000/index by 
-default).
+interval and all .goo packages served in the repo 'repo'.
+You can then point a client at http://localhost:8000/repo, or view 
+http://localhost:8000/repo/index in a browser.
 
 Improvements to this design would include only updating the repository on 
 a package change as well as providing and api for adding/removing packages.
--- server/gooserve.go
+++ server/gooserve.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"path"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -36,6 +37,7 @@ var (
 	verbose   = flag.Bool("verbose", false, "print info level logs to stdout")
 	systemLog = flag.Bool("system_log", false, "log to Linux Syslog or Windows Event Log")
 	port      = flag.Int("port", 8000, "listen port")
+	repoName  = flag.String("repo_name", "repo", "name of the repo to setup")
 
 	repoContents *repoPackages
 )
@@ -81,7 +83,7 @@ func packageInfo(pkgPath, packageDir string) error {
 	}
 	defer f.Close()
 
-	repoContents.add(filepath.Join(packageDir, pkg), goolib.Checksum(f), spec)
+	repoContents.add(path.Join(packageDir, pkg), goolib.Checksum(f), spec)
 	return nil
 }
 
@@ -96,8 +98,6 @@ func runSync(packageDir string) error {
 		return err
 	}
 
-	// TODO(ajackura): Don't reset repoContents each run, just append
-	// or remove as needed.
 	repoContents = &repoPackages{}
 	var wg sync.WaitGroup
 	for _, pkg := range pkgs {
@@ -138,18 +138,19 @@ func main() {
 
 	logger.Init("GooServe", *verbose, *systemLog, ioutil.Discard)
 
-	http.HandleFunc("/index", serve)
+	packageDir := filepath.Join(*root, "packages")
+	if err := runSync(packageDir); err != nil {
+		logger.Error(err)
+	}
+
+	http.HandleFunc(fmt.Sprintf("/%s/index", *repoName), serve)
+	http.Handle("/packages/", http.StripPrefix("/packages/", http.FileServer(http.Dir(packageDir))))
 	go func() {
 		err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 		if err != nil {
 			logger.Fatal(err)
 		}
 	}()
-
-	packageDir := filepath.Join(*root, "packages")
-	if err := runSync(packageDir); err != nil {
-		logger.Error(err)
-	}
 
 	for range time.Tick(*interval) {
 		if err := runSync(packageDir); err != nil {
